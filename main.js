b3a704b90672f872a9b391b826ef17131ee6ff2e
mission bit <missionbit@missions-MacBook-Air-7.local>
2018-07-24 10:14:40 -0700

    removed ground

--- main.js
+++ main.js
@@ -5,10 +5,10 @@ function preload() {
     game.load.image('xiomara-tile', 'owlishmedia_pixel_tiles.png');
     
     game.load.image('sky', 'assets/sky.png');
-    game.load.image('ground', 'assets/platform.png');
+   
     game.load.image('star', 'assets/star.png');
-  
-game.load.spritesheet('bmonkey', 'assets/(1234)monkeyanimation.png', 32, 32);
+   game.load.spritesheet('tiger', 'assets/tiger.png', 32, 26);
+    game.load.spritesheet('bmonkey', 'assets/(1234)monkeyanimation.png', 32, 32);
 }
 
 // Any variables that we want to use in both create() and update()
@@ -20,9 +20,10 @@ var platforms;
 var map;
 var blockingLayer;
 function create() {
-
-
-    //  A simple background for our gam
+    
+    
+    
+    //  A simple background for our game
     game.add.sprite(0, 0, 'sky');
     
     //setting up map 
@@ -35,21 +36,17 @@ function create() {
     game.add.sprite(20, 20, 'star');
     
     
+
     
-    //  The platforms group contains the ground and the 2 ledges we can jump on
-    platforms = game.add.group();
+    // computer and its settings
+    tiger = game.add.sprite (166, 205, 'tiger');
+     tiger.scale.setTo(2, 2);
     
-    // Here we create the ground.
-    ground = platforms.create(0, game.world.height - 64, 'ground');
-    
-    //  Double the size of the platform (vertically by 2 and horizontally by 2)
-    ground.scale.setTo(2, 2);
-    
-    //  We need to enable physics on the ground so that it can move and collide with stuff
-    game.physics.arcade.enable(ground);
-    
-    //  This stops the ground from falling away when you jump on it
-    ground.body.immovable = true;
+     // Add animations to the computer
+    tiger.animations.add('left', [ 4, 5, 6], 10, true);
+    tiger.animations.add('right', [ 1, 2, 3,], 10, true); 
+      game.physics.arcade.enable(tiger);
+    tiger.body.gravity.y = 300;
     
     // The player and its settings
     player = game.add.sprite(32, game.world.height - 360, 'bmonkey');
@@ -67,24 +64,24 @@ function create() {
     player.animations.add('left', [ 1, 2, 3, 4, 5], 10, true);
     player.animations.add('right', [ 7, 9, 7, 9], 10, true);
     
-    //  Now let's create two ledges
-    var ledge1 = platforms.create(400, 400, 'ground');
-    var ledge2 = platforms.create(-150, 250, 'ground');
-    
-    // Enable physics on the platforms so you can collide with them
-    game.physics.arcade.enable(platforms);
-    
+
     // Prevent the ledges from moving
     ledge1.body.immovable = true;
     ledge2.body.immovable = true;
 }
 
+
+
+
+
+
+
+
+
 function update() {
-    
     game.physics.arcade.collide(player,blockingLayer);
-    // Check for collisions between the player and the ground
-    game.physics.arcade.collide(player, ground);
-    
+    game.physics.arcade.collide(tiger,blockingLayer);
+   
     // Check for collisions between the player and all platforms
     game.physics.arcade.collide(player, platforms);
     

9bacf0bd9250db9dfaa5967e2491778030b8ed3f
Wugod1852 <Wugod1852@github>
2018-07-26 11:46:31 -0700

    fix main menu and how to play

--- pages/howtoplay.js
+++ pages/howtoplay.js
@@ -2,18 +2,26 @@ var howtoplayState = {
 
     preload: function(){
         
-    game.load.image('tiles', 'curious-george51.jpg'); 
+        game.load.image('how to play image', 'curious-george51.jpg'); 
         
     },
     create: function() {
         
-    var style = { font: "bold 32px Arial", fill: "#fff", boundsAlignH: "left", boundsAlignV: "middle" };
-    
-    text = game.add.text(40, 100, "Left arrow key is to go left", style);
-    text = game.add.text(40, 300, "Right arrow key is to go right", style);
-    text = game.add.text(40, 500, "Spacebar key is to jump", style);
-    
-    text.inputEnabled = true;
+        var style = { font: "bold 32px Arial", fill: "#fff", boundsAlignH: "left", boundsAlignV: "middle" };
+
+        background = game.add.sprite(0, 0, 'how to play image');
+        background.scale.setTo(1.7, 1.7)
+
+        text = game.add.text(40, 100, "Left arrow key is to go left", style);
+        text = game.add.text(40, 300, "Right arrow key is to go right", style);
+        text = game.add.text(40, 500, "Spacebar key is to jump", style);
+        text = game.add.text(40, 650, "Back to main menu", style);
+
+
+        text.inputEnabled = true;
+        text.events.onInputDown.add(function() {
+            game.state.start('mainmenu');
+        });
     
     }
 }
